0d442b751cf96d3955f1a5b040c4141fae7285e3
Zach Dennis <zach.dennis@gmail.com>
2012-09-11 09:04:50 -0400

    including instructions macro

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,11 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  config.extend Module.new {
+    def instructions(str)
+    end
+  }
 end
+
+require 'pry'
