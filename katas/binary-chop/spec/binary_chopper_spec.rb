3bfa8129631a03d4784885d8ef89ff46b212dcc6
Zach Dennis <zach.dennis@gmail.com>
2013-04-28 14:10:19 -0400

    Adding additional assertions/examples to ensure that solutions don't fall into a gap of false positives.

--- katas/binary-chop/spec/binary_chopper_spec.rb
+++ katas/binary-chop/spec/binary_chopper_spec.rb
@@ -10,11 +10,25 @@ shared_examples_for "binary chop" do
       expect(chop(3, [1, 3, 5])).to eq(1)
       expect(chop(5, [1, 3, 5])).to eq(2)
 
-      expect(chop(1, [1, 3, 5, 7])).to eq(0)
+      expect(chop(1, [1, 3, 5, 7, 9, 11, 13, 15, 17])).to eq(0)
       expect(chop(3, [1, 3, 5, 7])).to eq(1)
       expect(chop(5, [1, 3, 5, 7])).to eq(2)
       expect(chop(7, [1, 3, 5, 7])).to eq(3)
+
+      expect(chop(1,  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(0)
+      expect(chop(3,  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(1)
+      expect(chop(5,  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(2)
+      expect(chop(7,  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(3)
+      expect(chop(9,  [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(4)
+      expect(chop(11, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(5)
+      expect(chop(13, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(6)
+      expect(chop(15, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(7)
+      expect(chop(17, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(8)
+      expect(chop(19, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(9)
+      expect(chop(21, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(10)
+      expect(chop(23, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(11)
     end
+
   end
 
   context "and the search target doesn't exist in the collection" do
@@ -32,6 +46,8 @@ shared_examples_for "binary chop" do
       expect(chop(4, [1, 3, 5, 7])).to eq(-1)
       expect(chop(6, [1, 3, 5, 7])).to eq(-1)
       expect(chop(8, [1, 3, 5, 7])).to eq(-1)
+
+      expect(chop(8, [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23])).to eq(-1)
     end
   end
 end
